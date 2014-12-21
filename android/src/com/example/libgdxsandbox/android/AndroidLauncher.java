606b464b3f1e39463a4cc6850e8c676c72a232ea
stenman <roger.stenman@gmail.com>
2014-12-21 13:07:38 +0100

    Initial commit

---
+++ android/src/com/example/libgdxsandbox/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.example.libgdxsandbox.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.example.libgdxsandbox.LibgdxSandbox;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LibgdxSandbox(), config);
+	}
+}
---
+++ html/src/com/example/libgdxsandbox/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.example.libgdxsandbox.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.example.libgdxsandbox.LibgdxSandbox;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LibgdxSandbox();
+        }
+}
